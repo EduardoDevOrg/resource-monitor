d0fff195ef3f426bad3f4df6d6373318cec3e855
eduardosantos1989 <eduardo1989.labor1@gmail.com>
2024-08-06 05:49:13 +0200

    resource-monitor-fully-featured-v1

--- src/modules/config.rs
+++ src/modules/config.rs
@@ -4,11 +4,13 @@ use std::{collections::HashMap, env, fs, path::{Path, PathBuf}, process};
 #[allow(dead_code)]
 pub struct ConfigEntry {
     pub log_type: String,
-    pub location: String,
+    pub log_folder: PathBuf,
     pub interval: u64,
     pub bin_folder: PathBuf,
     pub app_folder: PathBuf,
     pub root_folder: PathBuf,
+    pub host: String,
+    pub port: u16,
 }
 fn get_app_dirs() -> (PathBuf, PathBuf, PathBuf) {
     let current_exe = env::current_exe().unwrap();
@@ -30,125 +32,156 @@ fn get_app_dirs() -> (PathBuf, PathBuf, PathBuf) {
     (bin_folder, app_folder, root_folder)
 }
 
+fn read_file(path: &Path, last_modified_time: &mut std::time::SystemTime) -> HashMap<String, HashMap<String, String>>{
+    let mut config_data = HashMap::new();
+    if path.is_file() {
+        if let Ok(metadata) = fs::metadata(path) {
+            if let Ok(modified_time) = metadata.modified() {
+                if modified_time > *last_modified_time {
+                    if let Ok(contents) = fs::read_to_string(path) {
+                        let mut section = String::new();
+                        let mut section_map = HashMap::new();
+
+                        for line in contents.lines() {
+                            let line = line.trim();
+                            if line.is_empty() || line.starts_with('#') {
+                                continue;
+                            }
+                            if line.starts_with('[') && line.ends_with(']') {
+                                if !section.is_empty() {
+                                    config_data.insert(section.clone(), section_map.clone());
+                                    section_map.clear();
+                                }
+                                section = line[1..line.len() - 1].to_string();
+                            } else if let Some((key, value)) = line.split_once('=') {
+                                section_map.insert(key.trim().to_string(), value.trim().to_string());
+                            }
+                        }
+
+                        if !section.is_empty() {
+                            config_data.insert(section, section_map);
+                        }
+                        *last_modified_time = modified_time;
+                    }
+                }
+            }
+        }
+    }
+    config_data
+}
+
+fn visit_dirs(dir: &Path) -> Option<PathBuf> {
+    // Search in the provided directory
+    let agent_conf_path = dir.join("agent.conf");
+    if agent_conf_path.exists() {
+        return Some(agent_conf_path);
+    }
+
+    // Go one directory up and search in `default` or `local`
+    if let Some(parent_dir) = dir.parent() {
+        let default_dir = parent_dir.join("default").join("agent.conf");
+        if default_dir.exists() {
+            return Some(default_dir);
+        }
+
+        let local_dir = parent_dir.join("local").join("agent.conf");
+        if local_dir.exists() {
+            return Some(local_dir);
+        }
+    }
+
+    // If no file is found, print a message and return None
+    println!("No agent.conf found");
+    None
+}
+
 fn read_config_file(config_path: &Path) -> HashMap<String, HashMap<String, String>> {
     let mut config_data = HashMap::new();
+    let config_path = visit_dirs(config_path);
     let mut last_modified_time = std::time::SystemTime::UNIX_EPOCH;
-
-    fn read_file(path: &Path, config_data: &mut HashMap<String, HashMap<String, String>>, last_modified_time: &mut std::time::SystemTime) {
-        if path.is_file() {
-            if let Ok(metadata) = fs::metadata(path) {
-                if let Ok(modified_time) = metadata.modified() {
-                    if modified_time > *last_modified_time {
-                        if let Ok(contents) = fs::read_to_string(path) {
-                            let mut section = String::new();
-                            let mut section_map = HashMap::new();
-
-                            for line in contents.lines() {
-                                let line = line.trim();
-                                if line.is_empty() || line.starts_with('#') {
-                                    continue;
-                                }
-                                if line.starts_with('[') && line.ends_with(']') {
-                                    if !section.is_empty() {
-                                        config_data.insert(section.clone(), section_map.clone());
-                                        section_map.clear();
-                                    }
-                                    section = line[1..line.len() - 1].to_string();
-                                } else if let Some((key, value)) = line.split_once('=') {
-                                    section_map.insert(key.trim().to_string(), value.trim().to_string());
-                                }
-                            }
-
-                            if !section.is_empty() {
-                                config_data.insert(section, section_map);
-                            }
-                            *last_modified_time = modified_time;
-                        }
-                    }
-                }
-            }
-        }
+    if let Some(config_path) = config_path {
+        config_data = read_file(&config_path, &mut last_modified_time);
     }
-
-    fn visit_dirs(dir: &Path, config_data: &mut HashMap<String, HashMap<String, String>>, last_modified_time: &mut std::time::SystemTime) {
-        if dir.is_dir() {
-            if let Ok(entries) = fs::read_dir(dir) {
-                for entry in entries.flatten() {
-                    let path = entry.path();
-                    if path.is_dir() {
-                        visit_dirs(&path, config_data, last_modified_time);
-                    } else if path.file_name().unwrap_or_default() == "splunkagent.conf" {
-                        read_file(&path, config_data, last_modified_time);
-                    }
-                }
-            }
-        }
-    }
-
-    visit_dirs(config_path, &mut config_data, &mut last_modified_time);
     config_data
 }
 
 pub fn get_configmap(module: &str) -> ConfigEntry {
     match module {
-        "startup" | "splunkagent" | "diskstats" | "storewatch" => {
+        "startup" | "agent" | "diskstats" | "storewatch" => {
             let (bin_folder, app_folder, root_folder) = get_app_dirs();
-    let config_data = read_config_file(&app_folder);
-    
-    let default_type = String::from("file");
-    let config_type = config_data
-        .get("default")
-        .and_then(|section| section.get("type"))
-        .unwrap_or(&default_type)
-        .to_string();
-    
-    let default_location = String::from("/var/log/splunk");
-    let location = config_data
-        .get("default")
-        .and_then(|section| section.get("location"))
-        .unwrap_or(&default_location)
-        .trim_start_matches('/')
-        .to_string();
-    
 
-    let default_interval = 10;
-    let interval: u64 = config_data
-        .get(module)
-        .and_then(|section| section.get("interval"))
-        .and_then(|s| s.parse().ok())
-        .unwrap_or(default_interval);
+            let config_data = read_config_file(&bin_folder);
+            
+            let default_type = String::from("file");
+            let config_type = config_data
+                .get("default")
+                .and_then(|section| section.get("type"))
+                .unwrap_or(&default_type)
+                .to_string();
+            
+            let default_location = String::from("/var/log/");
+            let location = config_data
+                .get("default")
+                .and_then(|section| section.get("location"))
+                .unwrap_or(&default_location)
+                .trim_start_matches('/')
+                .to_string();
 
-    if config_type == "file" {
-        let log_location = format!("{}/{}", root_folder.display(), location);
-        let log_folder = PathBuf::from(&log_location);
+            let default_port = 0;
+            let port: u16 = config_data
+                .get("default")
+                .and_then(|section| section.get("port"))
+                .and_then(|s| s.parse().ok())
+                .unwrap_or(default_port);
 
-        if !Path::new(&log_folder).exists() {
-            fs::create_dir_all(&log_folder).expect("Failed to create log directory");
-        }
+            let default_interval = 10;
+            let interval: u64 = config_data
+                .get(module)
+                .and_then(|section| section.get("interval"))
+                .and_then(|s| s.parse().ok())
+                .unwrap_or(default_interval);
 
-    } else if config_type == "tcp" {
-        println!("TCP type is not supported yet");
-        process::exit(1);
-    } else if config_type == "http" {
-        println!("HTTP type is not supported yet");
-        process::exit(1);
-    }
+            let log_location: String;
+            let mut log_folder = PathBuf::new(); // Initialize with a default value
+            let mut host = String::new();
 
-    
-    ConfigEntry {
-        log_type: config_type,
-        location,
-        interval,
-        bin_folder,
-        app_folder,
-        root_folder,
-    }
+            if config_type == "file" {
+                if root_folder.to_string_lossy().contains("splunk") || root_folder.to_string_lossy().contains("splunkforwarder") {
+                    log_location = format!("{}/{}", root_folder.display(), location);
+                    log_folder = PathBuf::from(&log_location);
+                } else {
+                    log_location = format!("/{}", location);
+                    log_folder = PathBuf::from(&log_location);
+                }
+            
+                if !Path::new(&log_folder).exists() {
+                    fs::create_dir_all(&log_folder).expect("Failed to create log directory");
+                }
+
+            } else if config_type == "tcp" {
+                println!("TCP type is not supported yet");
+                process::exit(1);
+            } else if config_type == "udp" {
+                host = location;
+            } else if config_type == "http" {
+                println!("HTTP type is not supported yet");
+                process::exit(1);
+            }
+
+            ConfigEntry {
+                log_type: config_type,
+                log_folder,
+                interval,
+                bin_folder,
+                app_folder,
+                root_folder,
+                host,
+                port
+            }
         },
         _ => {
-            eprintln!("Error: Invalid module '{}'. Valid modules are: startup, splunkagent, diskstats, storewatch", module);
+            eprintln!("Error: Invalid module '{}'. Valid modules are: startup, agent, diskstats, storewatch", module);
             process::exit(1);
         }
     }
-    
-    
 }
--- src/modules/log_entry.rs
+++ src/modules/log_entry.rs
@@ -1,15 +1,16 @@
 use std::{fs, path::Path};
-
 use serde::{Serialize, Deserialize};
+use serde_json::ser::Formatter;
 use sysinfo::{System, Networks};
+use std::io::Write;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct LogEntry {
     pub timestamp: u64,
-    cpu_usage: f32,
+    cpu_usage: f64,
     total_mem: u64,
     used_mem: u64,
-    mem_usage: f32,
+    mem_usage: f64,
     disk_read: u64,
     disk_write: u64,
     bytes_in: u64,
@@ -19,11 +20,22 @@ pub struct LogEntry {
     hostname: String,
     pub uptime: u64,
     pub component: String,
-    pub splunk_uptime: u64,
+    pub agent_uptime: u64,
+}
+
+struct CustomFormatter;
+
+impl Formatter for CustomFormatter {
+    fn write_f64<W>(&mut self, writer: &mut W, value: f64) -> std::io::Result<()>
+    where
+        W: Write + ?Sized,
+    {
+        write!(writer, "{:.3}", value)
+    }
 }
 
 impl LogEntry {
-    pub fn new(hostname: String, component: String, splunk_uptime: u64) -> Self {
+    pub fn new(hostname: String, component: String, agent_uptime: u64) -> Self {
         Self {
             timestamp: 0,
             cpu_usage: 0.0,
@@ -39,27 +51,12 @@ impl LogEntry {
             uptime: 0,
             hostname,
             component,
-            splunk_uptime,
+            agent_uptime,
         }
     }
 
-    pub fn reset(&mut self) {
-        self.timestamp = 0;
-        self.cpu_usage = 0.0;
-        self.total_mem = 0;
-        self.used_mem = 0;
-        self.mem_usage = 0.0;
-        self.disk_read = 0;
-        self.disk_write = 0;
-        self.bytes_in = 0;
-        self.bytes_out = 0;
-        self.packets_in = 0;
-        self.packets_out = 0;
-        self.uptime = 0;
-    }
-
     pub fn update(&mut self, sys: &System, networks: &Networks) {
-        for (_interface_name, network) in networks {
+        for (_interface_name, network) in networks.iter() {
             self.bytes_in += network.received();
             self.bytes_out += network.transmitted();
             self.packets_in += network.packets_received();
@@ -72,25 +69,44 @@ impl LogEntry {
             self.disk_write += disk_usage.written_bytes;
         }
 
-        self.cpu_usage += sys.global_cpu_info().cpu_usage();
+        self.cpu_usage += sys.global_cpu_usage() as f64;
 
         let total_mem = sys.total_memory();
         let used_mem = sys.used_memory();
-        self.total_mem += total_mem;
-        self.used_mem += used_mem;
-        self.mem_usage += used_mem as f32 / total_mem as f32 * 100.0;
+        self.total_mem = total_mem; // Set to the current value
+        self.used_mem = used_mem; // Set to the current value
+        self.mem_usage += used_mem as f64 / total_mem as f64 * 100.0;
     }
 
-    pub fn average(&mut self, interval: u64) {
-        self.bytes_in /= interval;
-        self.bytes_out /= interval;
-        self.packets_in /= interval;
-        self.packets_out /= interval;
-        self.cpu_usage /= interval as f32;
-        self.mem_usage /= interval as f32;
-        self.total_mem /= interval;
-        self.used_mem /= interval;
+    pub fn finalize(&mut self, sample_count: u64) {
+        self.cpu_usage /= sample_count as f64;
+        self.mem_usage /= sample_count as f64;
+        self.bytes_in /= sample_count;
+        self.bytes_out /= sample_count;
+        self.packets_in /= sample_count;
+        self.packets_out /= sample_count;
+        self.disk_read /= sample_count;
+        self.disk_write /= sample_count;
     }
+
+    pub fn reset(&mut self) {
+        self.cpu_usage = 0.0;
+        self.mem_usage = 0.0;
+        self.bytes_in = 0;
+        self.bytes_out = 0;
+        self.packets_in = 0;
+        self.packets_out = 0;
+        self.disk_read = 0;
+        self.disk_write = 0;
+    }
+
+    pub fn write_json<W: Write>(&self, writer: W) -> serde_json::Result<()> {
+        let mut serializer = serde_json::Serializer::with_formatter(writer, CustomFormatter);
+        self.serialize(&mut serializer)
+    }
+
+    
+
 }
 
 pub fn check_log_file_size(log_path: &Path) {
